a81846879f66b49ea1c143dbc7094b8a67c717a6
yurizhuravel <yuri@zhuravel.co.uk>
2025-06-12 10:44:20 +0100

    Simpler codec derivation

--- src/main/scala/ice/finance/Models.scala
+++ src/main/scala/ice/finance/Models.scala
@@ -8,17 +8,17 @@ trait AmountPerService
 case class ServiceCost(serviceId: Int, amount: Double) extends AmountPerService
 
 object ServiceCost:
-  implicit val serviceCostEncoder: Encoder[ServiceCost] = deriveEncoder[ServiceCost]
-  implicit val serviceCostDecoder: Decoder[ServiceCost] = deriveDecoder[ServiceCost]
+  implicit val serviceCostEncoder: Encoder[ServiceCost] = deriveEncoder
+  implicit val serviceCostDecoder: Decoder[ServiceCost] = deriveDecoder
 
 case class ServiceCommission(serviceId: Int, commission: Double) extends AmountPerService
 
 object ServiceCommission:
-  implicit val serviceCommissionEncoder: Encoder[ServiceCommission] = deriveEncoder[ServiceCommission]
-  implicit val serviceCommissionDecoder: Decoder[ServiceCommission] = deriveDecoder[ServiceCommission]
+  implicit val serviceCommissionEncoder: Encoder[ServiceCommission] = deriveEncoder
+  implicit val serviceCommissionDecoder: Decoder[ServiceCommission] = deriveDecoder
 
 case class ClientRequest(clientId: String, serviceCosts: List[ServiceCost])
 
 object ClientRequest:
-  implicit val clientRequestEncoder: Encoder[ClientRequest] = deriveEncoder[ClientRequest]
-  implicit val clientRequestDecoder: Decoder[ClientRequest] = deriveDecoder[ClientRequest]
+  implicit val clientRequestEncoder: Encoder[ClientRequest] = deriveEncoder
+  implicit val clientRequestDecoder: Decoder[ClientRequest] = deriveDecoder
